462b478aee00cd1c891834717126f6a539cff534
vrunoa <vruno.alassia@gmail.com>
2015-11-23 20:32:13 -0800

    add response console.log, grunt.log before uploading the app

--- tasks/testfairy.js
+++ tasks/testfairy.js
@@ -2,7 +2,7 @@
 var TestFairyUploader = require('../node_modules/testfairy/dist/TestFairyUploader.js')
 module.exports = function(grunt) {
   grunt.registerMultiTask('testfairy', 'Upload apps to Tesfairy', function(){
-    grunt.log.writeln("Uploading app to testfairy");
+    grunt.log.writeln("Uploading app to testfairy, this may take a while!");
     var uploader = new TestFairyUploader(this.options())
     var response = uploader.run()
     if(response[0] != null) {
